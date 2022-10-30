2c8983744b031ee7d772872c6e85eab8ecc4132c
Alessandro Candido <candido.ale@gmail.com>
2022-10-30 09:25:19 +0100

    Add web pages

---
+++ pages/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<main>
+    <section class="content">
+        <slot></slot>
+    </section>
+</main>
+
+<style>
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .content {
+        width: 60%;
+        margin-top: 2rem;
+    }
+
+    @media (max-width:800px) {
+        .content {
+            width: 80%;
+        }
+    }
+
+</style>
---
+++ pages/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>PhD Documents</h1>
+<ul>
+	<li><a href="/thesis.pdf">Thesis</a></li>
+	<li><a href="/activity-report.pdf">Activity Report</a></li>
+</ul>
