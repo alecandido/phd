8dd71067f5cf22a890cdc701e4629e275ac5a2a4
Alessandro Candido <candido.ale@gmail.com>
2022-11-17 12:39:38 +0100

    Convert rest abbreviations and math role

--- thesis/sections/from-rest.bash
+++ thesis/sections/from-rest.bash
@@ -10,8 +10,14 @@ for texinput in $@; do
   input=$(basename $texinput)
 
   # convert rest abbreviations to macros
-  sd '|(\w*?)|' '\\#$1\n' $input
-  sed -e 's@\\#@\L\1@gi' -i $input
+  sd '\|(\w*?)\|' '\\#$1\n' $input
+  sed -e 's@\\#\(.*\)@\\\L\1@g' -i $input
+
+  # translate inline math
+  sd ':math:`(.*?)`' '$ $1$' $input
+  sd '\$ ' '$' $input
+
+  echo "finished processing $texinput"
 done
 
 popd >/dev/null
